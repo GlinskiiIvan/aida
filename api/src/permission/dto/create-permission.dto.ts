4496c063ef3bcdb1e822231e237c1af5f76c30f0
Glinsky Ivan <glinsky_ivan@mail.ru>
2026-05-26 21:24:32 +0500

    добавил dto для permission

--- api/src/permission/dto/create-permission.dto.ts
+++ api/src/permission/dto/create-permission.dto.ts
@@ -1,1 +1,13 @@
-export class CreatePermissionDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
+
+export class CreatePermissionDto {
+  @ApiProperty({ example: 'patient:create', description: 'Значение разрешения' })
+  @IsString({ message: 'Должно быть строкой' })
+  readonly value: string;
+
+  @ApiProperty({ example: 'Разрешение на создание пациента', description: 'Описание разрешения', required: false})
+  @IsOptional()
+  @IsString({ message: 'Должно быть строкой' })
+  readonly description?: string;
+}
